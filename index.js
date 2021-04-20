a6522e0547f157ff186d199d10d8ac7c693be593
ahmed-saifulla <ahsaifulla@gmail.com>
2021-04-21 00:25:09 +0530

    basic socke io

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
+// const mongoose = require("mongoose");
 const server = require("http").createServer();
+// const Msg = require("./models/messages")
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
 const PORT = 4000;
+
+// mongoose.connect('mongodb://localhost:27017/chatApp', {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// }).then(done => {
+//     console.log('connected to mongo');
+// });
+
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+// const EXISTING_CHAT_MESSAGE_EVENT = "existingChatMessage";
 
 io.on("connection", (socket) => {
-  
-  // Join a conversation
-  const { roomId } = socket.handshake.query;
-  socket.join(roomId);
 
-  // Listen for new messages
-  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-  });
+    // Msg.find().then((result) => {
+    //     socket.emit(EXISTING_CHAT_MESSAGE_EVENT, result);
+    // })
 
-  // Leave the room if the user closes the socket
-  socket.on("disconnect", () => {
-    socket.leave(roomId);
-  });
+    // Join a conversation
+    const { roomId } = socket.handshake.query;
+    socket.join(roomId);
+
+    // Listen for new messages
+    socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+        // const message = new Msg({ body: data.body, roomId : data.roomId , senderId : data.senderId })
+        // message.save().then(() => {
+        io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+        // })
+    });
+
+    // Leave the room if the user closes the socket
+    socket.on("disconnect", () => {
+        socket.leave(roomId);
+    });
 });
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
---
+++ models/rooms.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const msgSchema = new mongoose.Schema({
+    body : {
+        type : String,
+        required : true
+    },
+    senderId : {
+        type : String,
+        required : true
+    }
+})
+
+const Msg = mongoose.model('msg', msgSchema);
+module.exports = Msg;
